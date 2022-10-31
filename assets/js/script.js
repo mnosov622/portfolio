5dccbebca88d4b2fe21dad35861fa2882499f3fb
Maxim N <mnosov622@gmail.com>
2022-10-31 07:56:17 +0700

    mobile added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-// $(function() {                       
-//     $(".header__contact-btn").click(() => { 
-        
+// $(function() {
+//     $(".header__contact-btn").click(() => {
+
 //       $(".header__contacts-content").addClass("open-contacts-content");
 //       $(".header__contacts").addClass("open-contacts");
 //     });
 
 //     $(".close-icon").click(() => {
-//         $(".header__contacts-content").removeClass("open-contacts-content"); 
+//         $(".header__contacts-content").removeClass("open-contacts-content");
 //         $(".header__contacts").removeClass("open-contacts");
 //     })
 
@@ -15,6 +15,7 @@
 //     });
 
 // });
+AOS.init();
 
 const contactMeBtn = document.querySelector(".header__contact-btn");
 const contactsContent = document.querySelector(".header__contacts-content");
@@ -22,23 +23,23 @@ const contacts = document.querySelector(".header__contacts");
 const closeIcon = document.querySelector(".close-icon");
 
 contactMeBtn.addEventListener("click", () => {
-    contacts.classList.remove("close-contacts");
-    contactsContent.classList.remove("close-contacts-content");
-    contactsContent.classList.add("open-contacts-content");
-    contacts.classList.add("open-contacts");
-    document.body.style.overflow = "hidden";
-})
+  contacts.classList.remove("close-contacts");
+  contactsContent.classList.remove("close-contacts-content");
+  contactsContent.classList.add("open-contacts-content");
+  contacts.classList.add("open-contacts");
+  document.body.style.overflow = "hidden";
+});
 
 closeIcon.addEventListener("click", () => {
+  contacts.classList.add("close-contacts");
+  contactsContent.classList.add("close-contacts-content");
+  document.body.style.overflow = "unset";
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target === contacts) {
     contacts.classList.add("close-contacts");
     contactsContent.classList.add("close-contacts-content");
     document.body.style.overflow = "unset";
-})
-
-document.addEventListener("click", (e) => {
-    if(e.target === contacts) {
-        contacts.classList.add("close-contacts");
-        contactsContent.classList.add("close-contacts-content");
-        document.body.style.overflow = "unset";
-    }
-})
+  }
+});
